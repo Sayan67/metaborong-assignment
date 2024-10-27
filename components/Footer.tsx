f78727d03c571cf9c37d66b890e71ec2ca9f39d0
Sayan67 <sayan.das.21@aot.edu.in>
2024-10-28 01:49:29 +0530

    Add : Footer, Fix: Navbar

---
+++ components/Footer.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='text-white/60 w-full absolute bottom-0 sm:px-20 px-8'>
+            <div className='flex justify-between w-full flex-col sm:flex-row gap-6'>
+                <div className='space-y-7 sm:w-[27rem]'>
+                    <Image src={'/logos/footerLogo.svg'} width={130} height={40} alt='footer logo' />
+                    <h1>The ultimate platform for turning coins into long-term gains. Stake, hold, and grow your assets with strategies designed for degen investors</h1>
+                </div>
+                <div>
+                    <h1 className='font-dx-bold text-white text-2xl'>Product</h1>
+                    <div className='flex flex-col gap-1 mt-2'>
+                        <Link href={'/'}>About product</Link>
+                        <Link href={'/'}>Guide</Link>
+                        <Link href={'/'} className='flex items-center'>
+                            Join Discord
+                            <Image src={'/icons/discord_color.svg'} width={20} height={20} alt='discord link' className='w-8'/>
+                        </Link>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex justify-center pt-8 pb-6 w-full'>
+                <h1>2024 @metaborong all rights reserved</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,12 +8,12 @@ const Navbar = () => {
     return (
         <div className='absolute top-8 left-1/2 -translate-x-1/2 gap-2 sm:gap-3 flex px-2 sm:px-0'>
             <div className='rounded-lg flex gap-[30px] sm:gap-[60px] items-center justify-center bg-primary-blue w-fit sm:py-3 sm:pl-4 sm:pr-9 py-2 px-4 border-2 border-white box-border drop-shadow-outline-white'>
-                <Link href={'/'} className='w-20'>
+                <Link href={'/'} className='w-14 sm:w-20'>
                     <Image src='/logos/mainLogo.svg' width={73} height={32} alt='logo' className='w-20 flex-none'/>
                 </Link>
                 <div className='flex gap-6 sm:gap-8 items-center justify-center'>
                     {navElements.map((element, index) => (
-                        <Link href={element.link} key={index} className='flex gap-1 items-center justify-center w-5 sm:w-full'>
+                        <Link href={element.link} key={index} className='flex gap-1 items-center justify-center w-4 sm:w-full'>
                             {/* Using the icon property as a key of the icons */}
                             <Image src={icons[element.icon as keyof typeof icons]} width={20} height={20} alt='icon' className='w-5'/>
                             <p className='text-white font-neue-medium text-xs sm:text-base hidden sm:flex'>{element.name}</p>
@@ -21,7 +21,7 @@ const Navbar = () => {
                     ))}
                 </div>
             </div>
-            <Link href={'/'} className='bg-discord-violate rounded-xl border border-white drop-shadow-outline-white flex items-center justify-center sm:w-16 w-14 h-fit py-3'>
+            <Link href={'/'} className='bg-discord-violate rounded-xl border border-white drop-shadow-outline-white flex items-center justify-center sm:w-16 w-12 h-fit py-3'>
                 <Image src={icons.discord} width={32} height={32} alt='discord logo' className='sm:w-8 w-6'/>
             </Link>
         </div>
