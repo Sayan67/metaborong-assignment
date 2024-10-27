a3580f0b9a9724ac83f5603517ddcfd090a6530f
Sayan67 <sayan.das.21@aot.edu.in>
2024-10-28 01:22:46 +0530

    "Navbar responsive"

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,23 +6,23 @@ import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <div className='absolute top-8 left-1/2 -translate-x-1/2 flex gap-3'>
-            <div className='rounded-lg flex gap-[60px] items-center justify-center bg-primary-blue w-fit py-3 pl-4 pr-9 border-2 border-white box-border drop-shadow-outline-white'>
-                <Link href={'/'}>
-                    <Image src='/logos/mainLogo.svg' width={73} height={32} alt='logo' />
+        <div className='absolute top-8 left-1/2 -translate-x-1/2 gap-2 sm:gap-3 flex px-2 sm:px-0'>
+            <div className='rounded-lg flex gap-[30px] sm:gap-[60px] items-center justify-center bg-primary-blue w-fit sm:py-3 sm:pl-4 sm:pr-9 py-2 px-4 border-2 border-white box-border drop-shadow-outline-white'>
+                <Link href={'/'} className='w-20'>
+                    <Image src='/logos/mainLogo.svg' width={73} height={32} alt='logo' className='w-20 flex-none'/>
                 </Link>
-                <div className='flex gap-8 items-center justify-center'>
+                <div className='flex gap-6 sm:gap-8 items-center justify-center'>
                     {navElements.map((element, index) => (
-                        <Link href={element.link} key={index} className='flex gap-1 items-center justify-center'>
+                        <Link href={element.link} key={index} className='flex gap-1 items-center justify-center w-5 sm:w-full'>
                             {/* Using the icon property as a key of the icons */}
-                            <Image src={icons[element.icon as keyof typeof icons]} width={20} height={20} alt='icon' />
-                            <p className='text-white font-neue-medium text-sm'>{element.name}</p>
+                            <Image src={icons[element.icon as keyof typeof icons]} width={20} height={20} alt='icon' className='w-5'/>
+                            <p className='text-white font-neue-medium text-xs sm:text-base hidden sm:flex'>{element.name}</p>
                         </Link>
                     ))}
                 </div>
             </div>
-            <Link href={'/'} className='bg-discord-violate rounded-xl border border-white drop-shadow-outline-white flex items-center justify-center w-16'>
-                <Image src={icons.discord} width={32} height={32} alt='discord logo' />
+            <Link href={'/'} className='bg-discord-violate rounded-xl border border-white drop-shadow-outline-white flex items-center justify-center sm:w-16 w-14 h-fit py-3'>
+                <Image src={icons.discord} width={32} height={32} alt='discord logo' className='sm:w-8 w-6'/>
             </Link>
         </div>
     )
